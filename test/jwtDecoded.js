7c3932d96d4dc5e7ddb222ee3593559bf82942ce
mhailstone <matthew.hailstone@gmail.com>
2016-11-11 16:25:07 -0700

    Catch and log to the console the error from the promise

--- test/jwtDecoded.js
+++ test/jwtDecoded.js
@@ -1,28 +1,42 @@
-/**
- * Created by martin on 5/17/16.
+/*
+ * Copyright 2016 Brigham Young University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 var assert = require('assert');
 var byuJwt = require('../index');
 
-describe('byuJWTTests', function() {
-    it('jwtDecoded', function (done) {
-        //to run test case capture a jwt and copy in the function invokation below.
-        byuJwt.jwtDecoded('jwtgoes here', 'well known url goes here')
-            .then(function (jwtDecoded)
-            {
-                try
-                {
-                    assert.equal(jwtDecoded.byu.client_net_id, '?');
-                    assert.equal(jwtDecoded.byu.resourceowner_net_id, '?');
-                    assert.equal(jwtDecoded.wso2.keytype, '?');
-                    done()
-                }
-                catch (e)
-                {
-                    console.log(e);
-                    done(err);
-                }
-            });
-    });
+describe('byuJWTTests', function () {
+  it('jwtDecoded', function (done) {
+    //to run test case capture a jwt and copy in the function invokation below.
+    byuJwt.jwtDecoded('jwtgoes here', 'well known url goes here')
+      .then(function (jwtDecoded) {
+        try {
+          assert.equal(jwtDecoded.byu.client.netId, '?');
+          assert.equal(jwtDecoded.byu.resourceOwner.netId, '?');
+          assert.equal(jwtDecoded.wso2.keyType, '?');
+          done()
+        }
+        catch (e) {
+          console.log(e);
+          done(e);
+        }
+      })
+      .catch(function (e) {
+        console.log(e);
+        done(e);
+      });
+  });
 });
