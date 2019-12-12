f19fa42105a18aa672ef7a6808af911e5e552dc6
Spencer Tuft <spencer.tuft@gmail.com>
2019-12-12 15:31:38 -0700

    Deleted cache file

--- cache.d.ts
+++
@@ -1,9 +0,0 @@
-interface CacheObject {
-  clearCache: () => void;
-  getCache: () => any;
-  setCache: (value: any) => void;
-  getTTL: () => number;
-  setTTL: (ttl: number) => void;
-}
-declare function Cache(): CacheObject;
-export default Cache;
