edb0c9bef2295ab24472b2b7931021d8331c68c0
Spencer Tuft <spencer.tuft@gmail.com>
2019-12-12 15:31:09 -0700

    Move d.ts files to root and merged d.ts files to one

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,30 @@
-export * from './lib'
+import {Request, Response, NextFunction} from 'express'
+
+// Cache Implementation
+interface CacheObject {
+  clearCache: () => void;
+  getCache: () => any;
+  setCache: (value: any) => void;
+  getTTL: () => number;
+  setTTL: (ttl: number) => void;
+}
+declare function Cache(): CacheObject;
+
+// ByuJWT Implementation
+declare function ByuJWT(options: any): any
+declare namespace ByuJWT {
+  const BYU_JWT_HEADER_CURRENT: string
+  const BYU_JWT_HEADER_ORIGINAL: string
+  const WELL_KNOWN_URL: string
+  function authenticate(options, cache: Cache, headers): Promise<any>
+  function authenticateUAPIMiddleware(req: Request, response: Response, next: NextFunction): any
+  function decodeJWT(options, cache: Cache, jwt: string): Promise<any>
+  function getOpenIdConfiguration(cache: Cache): Promise<any>
+  function getPublicKey(cache: Cache): Promise<any>
+  function verifyJWT(options, cache: Cache, jwt: string): Promise<any>
+  function AuthenticationError(message: any, error: any): void
+  function JsonWebTokenError(message: any, error: any): void
+  function NotBeforeError(message: any, date: any): void
+  function TokenExpiredError(message: any, expiredAt: any): void
+}
+export default ByuJWT
