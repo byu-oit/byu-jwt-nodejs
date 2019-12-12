56d7d592b4ab73a008313ee529924d64d8932fbc
Spencer Tuft <spencer.tuft@gmail.com>
2019-12-12 15:37:02 -0700

    Fix Cache type

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,13 @@
 import {Request, Response, NextFunction} from 'express'
 
 // Cache Implementation
-interface CacheObject {
+interface Cache {
   clearCache: () => void;
   getCache: () => any;
   setCache: (value: any) => void;
   getTTL: () => number;
   setTTL: (ttl: number) => void;
 }
-declare function Cache(): CacheObject;
 
 // ByuJWT Implementation
 declare function ByuJWT(options: any): any
@@ -16,12 +15,12 @@ declare namespace ByuJWT {
   const BYU_JWT_HEADER_CURRENT: string
   const BYU_JWT_HEADER_ORIGINAL: string
   const WELL_KNOWN_URL: string
-  function authenticate(options, cache: Cache, headers): Promise<any>
+  function authenticate(options: any, cache: Cache, headers: any): Promise<any>
   function authenticateUAPIMiddleware(req: Request, response: Response, next: NextFunction): any
-  function decodeJWT(options, cache: Cache, jwt: string): Promise<any>
+  function decodeJWT(options: any, cache: Cache, jwt: string): Promise<any>
   function getOpenIdConfiguration(cache: Cache): Promise<any>
   function getPublicKey(cache: Cache): Promise<any>
-  function verifyJWT(options, cache: Cache, jwt: string): Promise<any>
+  function verifyJWT(options: any, cache: Cache, jwt: string): Promise<any>
   function AuthenticationError(message: any, error: any): void
   function JsonWebTokenError(message: any, error: any): void
   function NotBeforeError(message: any, date: any): void
