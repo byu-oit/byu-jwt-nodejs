d59ff5cfd05736cde270a32e19857f0758decad1
GaryGSC <GaryGSC@users.noreply.github.com>
2021-09-01 10:42:46 -0600

    Use `async`/`await` in tests

--- test/index.test.js
+++ test/index.test.js
@@ -52,94 +52,83 @@ describe('byu-jwt', function () {
     })
   })
 
-  it('can get OpenID configuration', () => {
-    return byuJWT.getOpenIdConfiguration()
-      .then(config => {
-        expect(config).to.be.an.instanceOf(Object)
-        expect(config).to.have.ownProperty('issuer')
-      })
+  it('can get OpenID configuration', async () => {
+    const config = await byuJWT.getOpenIdConfiguration()
+    expect(config).to.be.an.instanceOf(Object)
+    expect(config).to.have.ownProperty('issuer')
   })
 
-  it('can get pem', () => {
-    return byuJWT.getPem()
-      .then(value => {
-        expect(value).to.match(/^-----BEGIN CERTIFICATE-----/)
-        expect(value).to.match(/-----END CERTIFICATE-----$/)
-      })
+  it('can get pem', async () => {
+    const value = await byuJWT.getPem()
+    expect(value).to.match(/^-----BEGIN CERTIFICATE-----/)
+    expect(value).to.match(/-----END CERTIFICATE-----$/)
   })
 
   describe('verifyJWT', () => {
-    it('valid JWT', () => {
-      return byuJWT.verifyJWT(jwt)
-        .then(value => {
-          expect(value).to.equal(true)
-        })
+    it('valid JWT', async () => {
+      const value = await byuJWT.verifyJWT(jwt)
+      expect(value).to.equal(true)
     })
 
-    it('invalid JWT', () => {
-      return byuJWT.verifyJWT('not-a-jwt')
-        .then(value => {
-          expect(value).to.equal(false)
-        })
+    it('invalid JWT', async () => {
+      const value = await byuJWT.verifyJWT('not-a-jwt')
+      expect(value).to.equal(false)
     })
 
-    it('invalid x5t in JWT', () => {
+    it('invalid x5t in JWT', async () => {
       const [encodedJwtHeaders, ...restOfJwt] = jwt.split('.')
       const decodedJwtHeaders = JSON.parse(Buffer.from(encodedJwtHeaders.replace(/=/g, ''), 'base64').toString())
       const jwtHeadersWithInvalidX5t = { ...decodedJwtHeaders, x5t: 'invalid x5t' }
       const encodedJwtHeadersWithInvalidX5t = Buffer.from(JSON.stringify(jwtHeadersWithInvalidX5t)).toString('base64').replace(/=/g, '')
       const jwtWithInvalidX5t = [encodedJwtHeadersWithInvalidX5t, ...restOfJwt].join('.')
-      return byuJWT.verifyJWT(jwtWithInvalidX5t)
-        .then(value => {
-          expect(value).to.equal(false)
-        })
+      const value = await byuJWT.verifyJWT(jwtWithInvalidX5t)
+      expect(value).to.equal(false)
     })
   })
 
   describe('decodeJWT', () => {
-    it('valid JWT', () => {
-      return byuJWT.decodeJWT(jwt)
-        .then(value => {
-          expect(value.client).to.have.ownProperty('byuId')
-        })
+    it('valid JWT', async () => {
+      const value = await byuJWT.decodeJWT(jwt)
+      expect(value.client).to.have.ownProperty('byuId')
     })
 
-    it('invalid JWT', () => {
-      return byuJWT.decodeJWT('not-a-jwt')
-        .then(() => { throw Error('not this error') })
-        .catch(err => {
-          expect(err.message).to.equal('jwt malformed')
-        })
+    it('invalid JWT', async () => {
+      try {
+        await byuJWT.decodeJWT('not-a-jwt')
+        throw Error('not this error')
+      } catch (err) {
+        expect(err.message).to.equal('jwt malformed')
+      }
     })
   })
 
   describe('authenticate', () => {
-    it('valid JWT', () => {
+    it('valid JWT', async () => {
       const headers = {}
       headers[ByuJWT.BYU_JWT_HEADER_CURRENT] = jwt
-      return byuJWT.authenticate(headers)
-        .then(result => {
-          expect(result.claims).to.deep.equal(result.current.client)
-        })
+      const result = await byuJWT.authenticate(headers)
+      expect(result.claims).to.deep.equal(result.current.client)
     })
 
-    it('invalid JWT', () => {
+    it('invalid JWT', async () => {
       const headers = {}
       headers[ByuJWT.BYU_JWT_HEADER_CURRENT] = 'not-a-jwt'
-      return byuJWT.authenticate(headers)
-        .then(() => { throw Error('not this error') })
-        .catch(err => {
-          expect(err.message).to.equal('Invalid JWT')
-        })
+      try {
+        await byuJWT.authenticate(headers)
+        throw Error('not this error')
+      } catch (err) {
+        expect(err.message).to.equal('Invalid JWT')
+      }
     })
 
-    it('missing JWT', () => {
+    it('missing JWT', async () => {
       const headers = {}
-      return byuJWT.authenticate(headers)
-        .then(() => { throw Error('not this error') })
-        .catch(err => {
-          expect(err.message).to.equal('Missing expected JWT')
-        })
+      try {
+        await byuJWT.authenticate(headers)
+        throw Error('not this error')
+      } catch (err) {
+        expect(err.message).to.equal('Missing expected JWT')
+      }
     })
   })
 
@@ -178,17 +167,15 @@ describe('byu-jwt', function () {
       })
     })
 
-    it('authenticate invalid JWT', () => {
+    it('authenticate invalid JWT', async () => {
       const headers = {}
       headers[ByuJWT.BYU_JWT_HEADER_CURRENT] = 'not-a-jwt'
       const req = { headers: headers }
       byuJWT.authenticateUAPIMiddleware(req, res, function () {
         throw Error('should not get here')
       })
-      return res.promise
-        .then(() => {
-          expect(res.code).to.equal(401)
-        })
+      await res.promise
+      expect(res.code).to.equal(401)
     })
   })
 })
