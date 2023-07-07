67eeb45b0d7260d8a3d49eb6fb9edf3984ab04a3
Spencer Tuft <stuft2@byu.edu>
2023-07-07 14:28:27 -0600

    fix: remove conversion from ms to seconds

--- packages/jwt/src/util/Date.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Converts milliseconds to seconds and rounds down to the nearest integer.
- *
- * @param ms - The number of milliseconds.
- * @returns The number of seconds.
- */
-export function toSeconds (ms: number): number {
-  return Math.floor(ms / 1000)
-}
